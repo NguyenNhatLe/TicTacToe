3a977af9fccc7631765abb7c5f1f58757967d89d
NguyenNhatLe <21020347@vnu.edu.vn>
2022-09-11 20:44:08 +0700

    bt

---
+++ Cell.java
@@ -0,0 +1,12 @@
+public class Cell{        
+   private CellStatus status;
+   public Cell(){
+       status = CellStatus.EMPTY;
+   }
+   public CellStatus getStatus(){
+       return status;
+   }
+   public void setStatus(CellStatus s){
+       status = s;
+   }
+}
---
+++ CellStatus.java
@@ -0,0 +1,7 @@
+enum CellStatus { 
+    EMPTY, X, O;
+    public static char asChar(CellStatus s){
+        if(s==X) return 'X';
+        else return 'O'; 
+    }
+}
